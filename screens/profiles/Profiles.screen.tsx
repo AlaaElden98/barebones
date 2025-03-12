9c7e22011e26acc1022a6c727c326ddc907c84c6
Alaa Abdelfattah <a.abdelfattah@zealpay.app>
2025-03-12 19:39:34 +0200

    update Profiles screen UI

--- screens/profiles/Profiles.screen.tsx
+++ screens/profiles/Profiles.screen.tsx
@@ -1,7 +1,16 @@
 import type React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
 
 import { usePets } from "@/hooks";
+import { PetCard } from "./components";
 import type { ProfilesScreenProps } from "@/navigation/types";
 
 export const ProfilesScreen: React.FC<ProfilesScreenProps> = ({
@@ -9,32 +18,78 @@ export const ProfilesScreen: React.FC<ProfilesScreenProps> = ({
   session,
 }) => {
   const { pets, loading, error } = usePets(session.user.id);
+
+  const handleOnPressProfile = (petId: string) => {
+    navigation.navigate("TabBar", { petId });
+  };
+
+  const handleOnPressAddProfile = () => {
+    navigation.navigate("AddProfile");
+  };
+
+  const EmptyComponent = () => (
+    <View style={styles.emptyContainer}>
+      {loading ? <ActivityIndicator size="large" color="#0000ff" /> : null}
+      {!loading && error ? <Text style={styles.errorText}>{error}</Text> : null}
+      {!loading && !error ? (
+        <Text style={styles.emptyText}>No profiles found</Text>
+      ) : null}
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Profiles</Text>
-      <Text>{JSON.stringify(pets)}</Text>
-      <Text>isLoading: {loading.toString()}</Text>
-      <Text>error: {error}</Text>
-      <Button
-        title="Add Profile"
-        onPress={() => navigation.navigate("AddProfile")}
+      <FlatList
+        data={pets}
+        renderItem={({ item }) => (
+          <PetCard pet={item} onPress={handleOnPressProfile} />
+        )}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={styles.contentContainer}
+        ListEmptyComponent={<EmptyComponent />}
       />
-      <Button
-        title="Go to Body Condition"
-        onPress={() =>
-          navigation.navigate("TabBar", {
-            petId: "3f4a9bde-8c2b-4f9e-ae3d-6d7f9b8c5e1a",
-          })
-        }
-      />
+      <TouchableOpacity
+        style={styles.addProfileButton}
+        onPress={handleOnPressAddProfile}
+        hitSlop={20}
+        activeOpacity={0.7}
+      >
+        <Text style={styles.addProfileButtonText}>Add New Profile</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  contentContainer: {
     flex: 1,
     padding: 16,
     backgroundColor: "#fff",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  addProfileButton: {
+    padding: 16,
+    marginBottom: 34,
+    alignSelf: "center",
+  },
+  addProfileButtonText: {
+    fontSize: 16,
+    color: "#007AFF",
+  },
+  emptyText: {
+    fontSize: 16,
+    color: "#000",
+  },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  errorText: {
+    fontSize: 16,
+    color: "red",
   },
 });
